11da88fe65996c6b3c2d27f9520e2a4455a5212f
citrahlzh <citrahalijah1992@gmail.com>
2026-02-16 15:54:47 +0700

    fix: scroll

--- resources/views/landing/layouts/app.blade.php
+++ resources/views/landing/layouts/app.blade.php
@@ -1,7 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="landing-scrollbar-hidden">
 <head>
     @include('landing.partials.header')
+
+    <style>
+        html.landing-scrollbar-hidden,
+        html.landing-scrollbar-hidden body {
+            -ms-overflow-style: none;
+            scrollbar-width: none;
+        }
+
+        html.landing-scrollbar-hidden::-webkit-scrollbar,
+        html.landing-scrollbar-hidden body::-webkit-scrollbar {
+            width: 0;
+            height: 0;
+            display: none;
+        }
+    </style>
 
     @stack('style')
 </head>
